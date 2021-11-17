5880a283ba7702f7cf4b0de4e210c49cfc6d2399
Luis Gabriel <lgcastellanos76@misena.edu.co>
2021-11-17 12:33:07 -0500

    update backpoffice build

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.939d8da6a5511d6491f2d740e22ae47f.js"
+  "/precache-manifest.dd3cf5f47ec781717b9ee66c7290c18f.js"
 );
 
 self.addEventListener('message', (event) => {
